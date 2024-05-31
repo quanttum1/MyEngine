e5a8dc53f1dd144b25bac0bae37e9bbefbcc6c8f
Taras Tuz <zutsarat@protonmail.com>
2024-06-01 01:02:36 +0300

    Add IntersectsWith method

--- Pseudo3DEngine.cs
+++ Pseudo3DEngine.cs
@@ -1,33 +1,35 @@
 using System.Numerics;
-using System.Math;
+using System;
 
 namespace MyEngine;
 
 class Pseudo3DEngine : IEngine
 {
-    private Vector2 _position = new Vector2(0, 0);
+    private Vector2 _position = new Vector2(0, 0); // Camera position
     private float _turnAngle = 0; // Camera's turn angle in degrees
-    private const float ViewAngle = 90;
-    private const float RayLength = 100;
+    private const float ViewAngle = 90; // Specifies how "many degrees" you can see
+    private const float RayLength = 100; // Specifies how far you can see
 
     private void RayCast(int x, int width)
     {
         double rayAngle = _turnAngle; // Raw camera's turn angle 
         rayAngle += ViewAngle * x / width; // Shifts the angle specificly for this ray
         rayAngle -= ViewAngle / 2; // Shifts back for a half of ViewAngle to make camera's turn at the middle of camera view
-        rayAngle *= PI / 180; // Converts to radians
+        rayAngle *= Math.PI / 180; // Converts to radians
 
-        Vector2 rayDirection = new Vector2(Cos(rayAngle), Sin(rayAngle));
-        rayDirection *= RayLength;
+        Vector2 rayDirection = new Vector2(Math.Cos(rayAngle), Math.Sin(rayAngle)); // Converts to vector
+        rayDirection *= RayLength; // Sets length of ray
 
-        Segment ray = new Segment(_position, _position + rayDirection);
+        Segment ray = new Segment(_position, _position + rayDirection); // Converts to segment
 
-        // TODO
+        Segment wall = new Segment(-2, 5, 2, 5); // Test wall
+
+        
     }
 
     public bool UpdateFrame(int width, int height)
     {
-        for (int i = 0; i < width; i++) {
+        for (int i = 0; i < width; i++) { // Goes trough all "colums" of the window
             RayCast(i, width); 
         }
         return true;
--- Segment.cs
+++ Segment.cs
@@ -1,5 +1,5 @@
 using System.Numerics;
-using System.Math;
+using System;
 
 namespace MyEngine;
 
@@ -20,8 +20,52 @@ class Segment
         Point2 = new Vector2(p2x, p2y);
     }
 
-    public Length()
+    public float Length()
     {
-        return Abs((Point2 - Point1).Length());
+        return Math.Abs((Point2 - Point1).Length());
+    }
+
+    public Vector2? IntersectsWith(Segment segment) // Returns the point where two segments cross or null if they don't or if they are equal
+    {
+        // If one of the segments is vertical, have to use other method, otherwise will be dividion by zero
+        if (Point1.X == Point2.X || segment.Point1.X == segment.Point2.X) 
+        {
+            bool segment1vert = Point1.X == Point2.X;
+            bool segment2vert = segment.Point1.X == segment.Point2.X;
+
+            if (segment1vert && segment2vert) return null; // If so they either don't cross or are equal
+            Segment NonVertical = segment1vert ? this : segment;
+            Segment Vertical = segment2vert ? this : segment;
+
+            float expr = (NonVertical.Point2.Y - NonVertical.Point1.Y) / (NonVertical.Point2.X - NonVertical.Point1.X);
+            
+            float crossX = Vertical.Point1.X;
+            float crossY = NonVertical.Point1.Y + crossX * expr - NonVertical.Point1.X * expr;
+
+            return new Vector2(crossX, crossY);
+        }
+
+        // Danger: complicated math area
+        float a1x = this.Point1.X;
+        float a1y = this.Point1.Y;
+
+        float b1x = this.Point2.X;
+        float b1y = this.Point2.Y;
+
+        float a1x = segment.Point1.X;
+        float a1y = segment.Point1.Y;
+
+        float b1x = segment.Point2.X;
+        float b1y = segment.Point2.Y;
+
+        float e1 = (b1y - a1y) / (b1x - a1x);
+        float e2 = (b2y - a2y) / (b2x - a2x);
+
+        if (e2 - e1 == 0) return null; 
+
+        float crossY = (a1y * e2 - a2y * e1 + a2x * e2 * e1 - a1x * e1 * e2) / (e2 - e1);
+        float crossX = -((a1y - a1x * e1 - crossY) / e1);
+
+        return new Vector2(crossX, crossY);
     }
 }
